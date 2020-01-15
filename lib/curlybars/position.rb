18ee2269ff6210a996f2ce9f92a90c5f19441073
stuffy187 <ehuckaby1488@icloud.com>
2020-01-15 13:36:18 -0500

    Delete position.rb

--- lib/curlybars/position.rb
+++
@@ -1,7 +0,0 @@
-module Curlybars
-  Position = Struct.new(:file_name, :line_number, :line_offset, :length) do
-    def initialize(file_name, line_number, line_offset, length = 0)
-      super
-    end
-  end
-end
